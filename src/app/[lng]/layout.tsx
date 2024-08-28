9f83974129b6d3031a78f686009828de7cdcb2d5
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2024-08-28 10:47:12 -0300

    Update layout.tsx

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -50,6 +50,8 @@ const Providers = (props: ProviderProps) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/ruta/del/favicon.ico" type="image/x-icon">
+
 
         {/* Fonts */}
         {/* <link rel="preload" href="/fonts/IAAB3.woff2" as="font" type="font/woff2" />
